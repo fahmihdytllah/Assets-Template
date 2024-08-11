0d5394353648b674734446dfbf1657cee2d3eb92
Fahmi Hdytllah <fahmicog@gmail.com>
2024-08-12 00:57:34 +0700

    fix bugs

--- jagobot/js/pages-bot.js
+++ jagobot/js/pages-bot.js
@@ -1,9 +1,19 @@
 $(document).ready(function () {
   loadBots();
-  
+
   $(document).on('click', '.bot-delete', function () {
     let idBot = $(this).data('id');
-    Swal.fire({ text: 'Apakah anda yakin ingin menghapus bot ini?', icon: 'warning', showCancelButton: true, confirmButtonText: 'Yes', customClass: { confirmButton: 'btn btn-primary waves-effect waves-light', cancelButton: 'btn btn-outline-danger ms-2 waves-effect waves-light' }, buttonsStyling: false }).then(function (result) {
+    Swal.fire({
+      text: 'Are you sure you want to remove this bot?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes',
+      customClass: {
+        confirmButton: 'btn btn-primary waves-effect waves-light',
+        cancelButton: 'btn btn-outline-danger ms-2 waves-effect waves-light',
+      },
+      buttonsStyling: false,
+    }).then(function (result) {
       if (result.value) {
         deleteBot(idBot);
       }
@@ -12,12 +22,16 @@ $(document).ready(function () {
 
   function loadBots() {
     $('#listBots').html('');
-    $('.card-table').block({ message: itemLoader, css: { backgroundColor: 'transparent', border: '0' }, overlayCSS: { backgroundColor: '#fff', opacity: 0.8 } });
+    $('.card-table').block({
+      message: itemLoader,
+      css: { backgroundColor: 'transparent', border: '0' },
+      overlayCSS: { backgroundColor: '#fff', opacity: 0.8 },
+    });
 
     $.get('bots?type=json', function (d) {
       $('.card-table').unblock();
-      
-      d.data.forEach(bot => {
+
+      d.data.forEach((bot) => {
         $('#listBots').append(`<tr>
           <td>
             <div class="d-flex justify-content-left align-items-center">
@@ -28,8 +42,11 @@ $(document).ready(function () {
               </div>
             </div>
           </td>
-          <td><span class="badge rounded-pill bg-label-${bot.status === 'Active' ? 'success' : 'danger'} me-1">${bot.status}</span></td>
-          <td><span class="badge rounded-pill bg-label-info me-1">${bot.platform}</span></td>
+          <td><span class="badge rounded-pill bg-label-${bot.status === 'Active' ? 'success' : 'danger'} me-1">${
+          bot.status
+        }</span></td>
+          <td><span class="badge rounded-pill bg-label-info me-1">${bot.keyId?.name}</span></td>
+          <td>${bot.platform}</td>
           <td>${bot.totalHitsPerDay}</td>
           <td>${bot.totalViewAdsPerDay}</td>
           <td>${bot.totalClickAdsPerDay}</td>
@@ -47,20 +64,36 @@ $(document).ready(function () {
   }
 
   function deleteBot(id) {
-    $.blockUI({ message: itemLoader, css: { backgroundColor: 'transparent', border: '0' }, overlayCSS: { backgroundColor: '#fff', opacity: 0.8 } });
+    $.blockUI({
+      message: itemLoader,
+      css: { backgroundColor: 'transparent', border: '0' },
+      overlayCSS: { backgroundColor: '#fff', opacity: 0.8 },
+    });
     $.ajax({
       url: 'bots?id=' + id,
       type: 'DELETE',
       success: function (d) {
         $.unblockUI();
         loadBots();
-        Swal.fire({ title: 'Good job!', text: d.msg, icon: 'success', customClass: { confirmButton: 'btn btn-primary waves-effect waves-light' }, buttonsStyling: !1 });
+        Swal.fire({
+          title: 'Good job!',
+          text: d.msg,
+          icon: 'success',
+          customClass: { confirmButton: 'btn btn-primary waves-effect waves-light' },
+          buttonsStyling: !1,
+        });
       },
       error: function (e) {
         $.unblockUI();
         let msg = e.responseJSON.msg;
-        Swal.fire({ title: 'Upss!', text: msg ? msg : 'There is an error!', icon: 'error', customClass: { confirmButton: 'btn btn-primary waves-effect waves-light' }, buttonsStyling: !1 });
-      }
+        Swal.fire({
+          title: 'Upss!',
+          text: msg ? msg : 'There is an error!',
+          icon: 'error',
+          customClass: { confirmButton: 'btn btn-primary waves-effect waves-light' },
+          buttonsStyling: !1,
+        });
+      },
     });
   }
 });
